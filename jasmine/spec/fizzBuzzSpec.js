c94226f5e7aa2c4d95cae4f06132d790996923fa
James <jamesdix@cwgsy.net>
2016-12-06 13:46:40 +0000

    isDivisivibleByFive feature added

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -17,4 +17,17 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
+
+  describe('knows when a number is', function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function() {
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
+
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -3,9 +3,17 @@ var Fizzbuzz = function() {
 };
 
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-  if(number%3 == 0 ){
+  if(number % 3 == 0){
     return true ; }
   else {
     return false ;
     };
   };
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  if(number % 5 == 0){
+    return true; }
+  else {
+    return false;
+  };
+};
